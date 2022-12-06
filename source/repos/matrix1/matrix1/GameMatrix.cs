c1e37dee3e199883bed99f181fff8544503a01ae
terp1s <piskova.ter@gmail.com>
2022-12-06 17:47:25 +0100

    dde

---
+++ source/repos/matrix1/matrix1/GameMatrix.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace matrix1
+{
+    /// <summary>
+    /// Represents game.
+    /// </summary>
+    class GameMatrix
+    {
+        int Rows { get; set; }
+        int Cols { get; set; }
+
+        Vect2d Start { get; set; }
+        Vect2d Finnish { get; set; }
+
+        int[,] matrix;
+
+        /// <summary>
+        /// Creates game.
+        /// </summary>
+        /// <param name="rows">Number of rows</param>
+        /// <param name="cols">Number of collumns</param>
+        /// <param name="start">Starting square</param>
+        /// <param name="finnish">Target square</param>
+        /// <param name="obstacles">list of obstacles coordinates</param>
+        public GameMatrix(int rows, int cols, Vect2d start, Vect2d finnish, List<Vect2d> obstacles)
+        {
+            Rows = rows;
+            Cols = cols;
+
+            matrix = new int[Rows, Cols];
+
+            // init
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Cols; j++)
+                {
+                    matrix[i, j] = int.MaxValue;
+                }
+            }
+
+            // set obstacles
+            foreach (Vect2d t in obstacles)
+            {
+                matrix[t.Row, t.Col] = -1;
+            }
+
+            matrix[start.Row, start.Col] = 0;
+
+            Start = start;
+            Finnish = finnish;
+        }
+
+        /// <summary>
+        /// from starting point using BFS explores matrix, counts number of steps from starting point to each square
+        /// </summary>
+        void FillMatrix()
+        {
+            // pokaždé, když na nějaké políčko zapíšeme hodnotu, tak ho dáme do fronty
+            Queue<Vect2d> que = new Queue<Vect2d>();
+            que.Enqueue(Start);
+
+            while (que.Count != 0)
+            {
+                // vezmeme první člen fronty
+                Vect2d position = que.Dequeue();
+                // a zpracujeme ho
+                MakeMoves(position, que);
+            }
+
+        }
+
+        /// <summary>
+        /// explores squares adjacent to position, if they are unexplored, writes number of steps and adds them to the que
+        /// </summary>
+        /// <param name="position"></param>
+        /// <param name="que"></param>
+        void MakeMoves(Vect2d position, Queue<Vect2d> que)
+        {
+            foreach(Vect2d vect in Vect2d.Moves())
+            {
+                if (matrix[vect.Row, vect.Col] > 0 && matrix[vect.Row, vect.Col] != int.MaxValue)
+                {
+                    que.Enqueue(vect);
+                    matrix[vect.Row, vect.Col] = matrix[position.Row, position.Col] + 1;
+                }
+            }
+        }
+    }
+}
+
---
+++ source/repos/matrix1/matrix1/Vect2d.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace matrix1
+{
+    class Vect2d
+    {
+        public int Row { get; private set; }
+        public int Col { get; private set; }
+        public int Hloubka { get; private set; }
+
+        public Vect2d(int row, int col)
+        {
+            Row = row;
+            Col = col;
+        }
+
+        /// <summary>
+        /// Returns sum of 2 vectors as a new vector 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static Vect2d Add(Vect2d a, Vect2d b)
+        {
+            return new Vect2d(a.Row + b.Row, a.Col + b.Col);
+        }
+
+        /// <summary>
+        /// Returns list of vectors with representing vertical and horizontal moves.
+        /// </summary>
+        /// <returns></returns>
+        public static List<Vect2d> Moves()
+        {
+            var l = new List<Vect2d>();
+
+            Vect2d nahoru = new Vect2d(-1, 0);
+            Vect2d dolu = new Vect2d(1, 0);
+            Vect2d doleva = new Vect2d(0, -1);
+            Vect2d doprava = new Vect2d(0, 1);
+            l.Add(nahoru);
+            l.Add(dolu);
+            l.Add(doleva);
+            l.Add(doprava);
+
+            return l;
+        }
+    }
+}
+
